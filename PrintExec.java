58bd87a9b861a45bb72981de4f4f57981b4e8d3e
TajouriSarra <sarra.tajouri@dauphine.eu>
2020-03-05 14:41:06 +0100

    Print

---
+++ PrintExec.java
@@ -0,0 +1,22 @@
+
+public class PrintExec {
+
+	public static void main(String[] args) {
+		
+		int nbreArg = args.length;
+		
+		for (int i=0; i<nbreArg-2; i++) {
+			System.out.print("cd "+ args[i]+ " /ln -sv");
+		}
+		System.out.print("cd "+ args[nbreArg-2]+ " / javac "+ args[args.length-1]+".java");
+		
+		if (args[nbreArg].equals("true")) {
+			System.out.print(" /mkdir bin");
+			System.out.println(" /mv -v"+ args[nbreArg-1]+ ".class bin");
+			
+		}
+		
+		System.out.println("java "+ args[nbreArg-1]);
+	}
+
+}
